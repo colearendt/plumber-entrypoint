794ca5b17309925b5034c56c454a8a8bc59e1ff7
Cole Arendt <cole.arendt@outlook.com>
2018-07-13 21:15:39 +0000

    initial pass at simple plumber entrypoint

---
+++ entrypoint-api/entrypoint.R
@@ -0,0 +1,9 @@
+library(plumber)
+pr <- plumb("plumber.R")
+pr$registerHook("exit", function(){
+  message("Bye bye!")
+  print("Bye bye!")
+  write("test-output", paste0(format(Sys.time(), "%Y%m%d_%H%M%S"),".txt"))
+})
+
+return(pr)
